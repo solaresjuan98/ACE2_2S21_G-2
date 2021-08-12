ea360fe997a416df6eae9d8e20205d42ab82a547
Juan Solares <solaresjuan43@gmail.com>
2021-08-11 20:39:49 -0600

    agregar libreria serialport

--- Practica1/backend/index.js
+++ Practica1/backend/index.js
@@ -1,1 +1,30 @@
-console.log('mc F')
+const express = require("express");
+const SerialPort = require("serialport");
+const ReadLine = require("@serialport/parser-readline");
+
+// Crear el servidor/aplicación de express
+const app = express();
+
+
+const port = new SerialPort("COM3", { baudRate: 9600 });
+const parser = port.pipe(new ReadLine({delimiter: "\n"}));
+
+port.on("open", () => {
+    console.log("Se abrió la comunicación");
+})
+
+port.on("data", (data) => {
+    console.log(data)
+} )
+
+// Peticion get
+app.get("/", (req, res) => {
+    res.json({
+        ok: true,
+        message: "Mensaje de prueba",
+    });
+});
+
+app.listen(4000, () => {
+    console.log(`Servidor corriendo en puerto ${4000}`);
+});
