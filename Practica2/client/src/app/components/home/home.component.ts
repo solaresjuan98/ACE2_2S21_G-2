6c1797d941ece4c15512990968fdb30d091a46b7
Diego <3006410600101@ingenieria.usac.edu.gt>
2021-10-02 18:44:28 -0600

    imagenes casí terminadas

--- Practica2/client/src/app/components/home/home.component.ts
+++ Practica2/client/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { parse } from 'path';
 import { ClimaService } from 'src/app/services/clima.service';
 
 @Component({
@@ -16,9 +17,8 @@ export class HomeComponent implements OnInit {
 
   velViento = ""
   visibilidad = ""
-  consin1 = ""
-  consin2 = ""
-  estatus = "Velocidad del viento " + this.velViento + ", Visibilidad " + this.visibilidad + ", " + this.consin1 + " lluvia y " + this.consin2 + " calor."
+  lluviacalor = ""
+  estatus = ""
 
   ultimoRegistro: any = {}
   registros: any = []
@@ -34,7 +34,7 @@ export class HomeComponent implements OnInit {
         this.iconoHumedad(this.ultimoRegistro.humedad);
         this.iconoVelocidad(this.ultimoRegistro.velocidad_viento);
         this.iconoDireccion(this.ultimoRegistro.direccion_viento);
-        this.iconoLuz(this.ultimoRegistro.iluminacion);
+        this.iconoLuz(this.ultimoRegistro.cantidad_luz);
         this.varStatus();*/
       })
       .catch((error) => {
@@ -42,8 +42,7 @@ export class HomeComponent implements OnInit {
       })
   }
 
-  iconoTemp(temp: string) {
-    let t = parseInt(temp, 10)
+  iconoTemp(t: number) {
     if (t < 0) {
       this.imgTemp = "https://i.imgur.com/zKv306G.png"
     } else if (t > 0 && t <= 8) {
@@ -63,32 +62,86 @@ export class HomeComponent implements OnInit {
     }
   }
 
-  iconoHumedad(hum: string) {
+  iconoHumedad(h: number) {
+    if (h < 15) {
+      this.imgHum = "https://i.imgur.com/oRCdtXo.png"
+    } else if (h >= 15 && h < 25) {
+      this.imgHum = "https://i.imgur.com/1fiVwfN.png"
+    } else if (h >= 25 && h < 40) {
+      this.imgHum = "https://i.imgur.com/tq2SqLG.png"
+    } else if (h >= 40 && h < 60) {
+      this.imgHum = "https://i.imgur.com/qadEwn1.png"
+    } else {
+      this.imgHum = "https://i.imgur.com/2yhMtOL.png"
+    }
+  }
+
+  iconoVelocidad(v: number) {
 
   }
 
-  iconoVelocidad(vel: string) {
-
-  }
-
-  iconoDireccion(dir: string) {
-    if (dir == "N") {
+  iconoDireccion(d: string) {
+    if (d == "Norte") {
       this.imgDir = "https://i.imgur.com/2lIuYxV.png"
-    } else if (dir == "S") {
+    } else if (d == "Sur") {
       this.imgDir = "https://i.imgur.com/8h3Mbes.png"
-    } else if (dir == "E") {
+    } else if (d == "Este") {
       this.imgDir = "https://i.imgur.com/7RrI9ln.png"
-    } else { //Oeste
+    } else if (d == "Oeste") {
       this.imgDir = "https://i.imgur.com/hPNorAf.png"
     }
   }
 
-  iconoLuz(luz: string) {
-
+  iconoLuz(l: number) {
+    if (l <= 10) {
+      this.imgLuz = "https://i.imgur.com/3VqD9FI.png"
+    } else if (l > 10 && l <= 30) {
+      this.imgLuz = "https://i.imgur.com/GcH9tOY.png"
+    } else if (l > 30 && l <= 50) {
+      this.imgLuz = "https://i.imgur.com/ujoBQfY.png"
+    } else if (l > 50 && l <= 70) {
+      this.imgLuz = "https://i.imgur.com/DgJgaJL.png"
+    } else {
+      this.imgLuz = "https://i.imgur.com/oRF5FoA.png"
+    }
   }
 
   varStatus() {
-    this.estatus = "Velocidad del viento " + this.velViento + ", Visibilidad " + this.visibilidad + ", " + this.consin1 + " lluvia y " + this.consin2 + " calor."
+    let promediovel: any;
+    let promediotemp: any;
+    this.climaService.velViento().toPromise().then((res) => {
+      let prom: any = res;
+      promediovel = prom.prom_vel_viento_global;
+    })
+
+    this.climaService.tempProm().toPromise().then((res) => {
+      let prom: any = res;
+      promediotemp = prom.prom_temperatura_global;
+    })
+
+    if (this.ultimoRegistro.velocidad_viento > promediovel) {
+      this.velViento = "alta"
+    } else {
+      this.velViento = "normal"
+    }
+
+    if (this.ultimoRegistro.cantidad_luz > 50) {
+      this.visibilidad = "despejada"
+    } else {
+      this.visibilidad = "nublada"
+    }
+
+    if (this.ultimoRegistro.temperatura >= promediotemp && this.ultimoRegistro.humedad >= 50) {
+      this.lluviacalor = "sin calor"
+    } else if (this.ultimoRegistro.temperatura < promediotemp && this.ultimoRegistro.humedad >= 50) {
+      this.lluviacalor = "con lluvia"
+    } else if (this.ultimoRegistro.temperatura >= promediotemp && this.ultimoRegistro.cantidad_luz < 50) {
+      this.lluviacalor = "con calor"
+    } else {
+      this.lluviacalor = "sin lluvia"
+    }
+
+    this.estatus = "Velocidad del viento " + this.velViento + ", Visibilidad " + this.visibilidad + ", " + this.lluviacalor + "."
   }
 
 }
