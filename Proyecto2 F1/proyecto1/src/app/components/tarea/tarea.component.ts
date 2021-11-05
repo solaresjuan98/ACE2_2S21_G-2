074b4e587a6f7654adab328d5a6d6592078c0b20
Diego <3006410600101@ingenieria.usac.edu.gt>
2021-11-05 16:11:23 -0600

    adios contador xD

--- Proyecto2 F1/proyecto1/src/app/components/tarea/tarea.component.ts
+++ Proyecto2 F1/proyecto1/src/app/components/tarea/tarea.component.ts
@@ -76,15 +76,15 @@ export class TareaComponent implements OnInit {
       minutos = time.getMinutes() + this.duracion;
     }
     if (this.int(horas) < 10) {
-      this.hora_final = `${time.getFullYear()}-${time.getMonth()+1}-${time.getDay()} 0${this.int(horas)}:${this.int(minutos)}:00`
+      this.hora_final = `${time.getFullYear()}-${time.getMonth()+1}-${time.getDate()} 0${this.int(horas)}:${this.int(minutos)}:00`
       if (this.int(minutos) < 10) {
-        this.hora_final = `${time.getFullYear()}-${time.getMonth()+1}-${time.getDay()} 0${this.int(horas)}:0${this.int(minutos)}:00`
+        this.hora_final = `${time.getFullYear()}-${time.getMonth()+1}-${time.getDate()} 0${this.int(horas)}:0${this.int(minutos)}:00`
       }
     } else if (this.int(minutos) < 10) {
-      this.hora_final = `${time.getFullYear()}-${time.getMonth()+1}-${time.getDay()} ${this.int(horas)}:0${this.int(minutos)}:00`
+      this.hora_final = `${time.getFullYear()}-${time.getMonth()+1}-${time.getDate()} ${this.int(horas)}:0${this.int(minutos)}:00`
     }
     else {
-      this.hora_final = `${time.getFullYear()}-${time.getMonth()+1}-${time.getDay()} ${this.int(horas)}:${this.int(minutos)}:00`
+      this.hora_final = `${time.getFullYear()}-${time.getMonth()+1}-${time.getDate()} ${this.int(horas)}:${this.int(minutos)}:00`
     }
 
   }
@@ -93,8 +93,8 @@ export class TareaComponent implements OnInit {
     let time = new Date();
     console.log(time)
     time.getTime();
-    this.fecha_registro = `${time.getFullYear()}-${time.getMonth()+1}-${time.getDay()}`;
-    this.hora_inicio = `${time.getFullYear()}-${time.getMonth()+1}-${time.getDay()} ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`
+    this.fecha_registro = `${time.getFullYear()}-${time.getMonth()+1}-${time.getDate()}`;
+    this.hora_inicio = `${time.getFullYear()}-${time.getMonth()+1}-${time.getDate()} ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`
     await this.getFinalTime(time);
     console.log(this.hora_final)
     console.log(this.hora_inicio)
